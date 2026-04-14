f8daa1feb0771b7f71ed6a390b647ead57b24093
Khizer Hussain <khizer.hussain@avanzainnovations.com>
2026-04-14 13:28:12 +0500

    remove txData from Testdoc struct

--- struct.go
+++ struct.go
@@ -4,7 +4,6 @@ type Testdoc struct {
 	ID        string `json:"id"`
 	Key       string `json:"key"`
 	Document  string `json:"document"`
-	TxData    string `json:"txData"` // full stringified payload
 	TxID      string `json:"txId"`
 	Timestamp string `json:"timestamp"`
 }
